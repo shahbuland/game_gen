dc128ecb537262545d9cd1de08c18f01dacdca8a
Shahbuland Matiana <smatiana1@gmail.com>
2024-03-11 22:56:53 +0000

    Teacher student feature matching distillation (wip)

---
+++ vae_distillation/adapt_diffusers.py
@@ -0,0 +1,160 @@
+"""
+This script is to adapt diffusers AutoencoderKL models to a format that can be trained
+"""
+
+from diffusers import AutoencoderKL
+from diffusers.models.autoencoders.vae import Encoder, Decoder
+from diffusers.models.autoencoders.vae import DiagonalGaussianDistribution
+
+import torch
+from torch import nn
+
+class HiddenStateEncoder(Encoder):
+    def forward(self, sample, output_hidden_states = False):
+        h = []
+
+        sample = self.conv_in(sample)
+        h.append(sample.clone())
+
+        if self.training and self.gradient_checkpointing:
+            raise ValueError("Gradient checkpointing not supported")
+        else:
+            # down
+            for down_block in self.down_blocks:
+                sample = down_block(sample)
+                h.append(sample.clone())
+
+            # middle
+            sample = self.mid_block(sample)
+            h.append(sample.clone())
+
+        # post-process
+        sample = self.conv_norm_out(sample)
+        sample = self.conv_act(sample)
+        sample = self.conv_out(sample)
+
+        if output_hidden_states:
+            return sample, h
+        else:
+            return sample
+
+    @classmethod
+    def cast(cls, model : Encoder):
+        # Make a dummy instance of HiddenStateEncoder then fill all the attributes after
+        model_new = cls()
+
+        model_new.layers_per_block = model.layers_per_block
+        model_new.conv_in = model.conv_in
+        model_new.mid_block = model.mid_block
+        model_new.down_blocks = model.down_blocks
+        model_new.conv_norm_out = model.conv_norm_out
+        model_new.conv_act = model.conv_act
+        model_new.conv_out = model.conv_out
+
+        return model_new
+
+class HiddenStateDecoder(Decoder):
+    def forward(self, sample, output_hidden_states = False):
+        h = []
+
+        sample = self.conv_in(sample)
+        h.append(sample.clone())
+
+        upscale_dtype = next(iter(self.up_blocks.parameters())).dtype
+        if self.training and self.gradient_checkpointing:
+            raise ValueError("Gradient checkpointing not supported")
+        else:
+            # middle
+            sample = self.mid_block(sample)
+            h.append(sample.clone())
+            sample = sample.to(upscale_dtype)
+
+            # up
+            for up_block in self.up_blocks:
+                sample = up_block(sample)
+                h.append(sample.clone())
+
+        # post-process
+        sample = self.conv_norm_out(sample)
+        sample = self.conv_act(sample)
+        sample = self.conv_out(sample)
+
+        if output_hidden_states:
+            return sample, h
+        else:
+            return sample
+
+    @classmethod
+    def cast(cls, model : Decoder):
+        # Make a dummy instance of HiddenStateDecoder then fill all the attributes after
+        model_new = cls()
+
+        model_new.layers_per_block = model.layers_per_block
+        model_new.conv_in = model.conv_in
+        model_new.mid_block = model.mid_block
+        model_new.up_blocks = model.up_blocks
+        model_new.conv_norm_out = model.conv_norm_out
+        model_new.conv_act = model.conv_act
+        model_new.conv_out = model.conv_out
+
+        return model_new
+
+class HiddenStateAutoencoderKL(nn.Module):
+    def __init__(
+        self,
+        encoder,
+        decoder,
+        quant_conv,
+        post_quant_conv
+    ):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+
+        self.quant_conv = quant_conv
+        self.post_quant_conv = post_quant_conv
+    
+    @classmethod
+    def from_pretrained(cls, model_path):
+        model = AutoencoderKL.from_pretrained(model_path)
+        return HiddenStateAutoencoderKL.cast(model)
+
+    @classmethod
+    def from_single_file(cls, model_path):
+        model = AutoencoderKL.from_single_file(model_path)
+        return HiddenStateAutoencoderKL.cast(model)
+    
+    @classmethod
+    def cast(cls, model : AutoencoderKL):
+        encoder = HiddenStateEncoder.cast(model.encoder)
+        decoder = HiddenStateDecoder.cast(model.decoder)
+
+        quant_conv = model.quant_conv
+        post_quant_conv = model.post_quant_conv
+
+        return cls(encoder, decoder, quant_conv, post_quant_conv)     
+
+    def encode(self, sample):
+        x, h = self.encoder(sample, output_hidden_states = True)
+        moments = self.quant_conv(x)
+        posterior = DiagonalGaussianDistribution(moments)
+        
+        return posterior, h
+
+    def decode(self, sample):
+        sample = self.post_quant_conv(sample)
+        x, h = self.decoder(sample, output_hidden_states = True)
+
+        return x, h
+
+if __name__ == "__main__":
+    url = "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors"
+    model = HiddenStateAutoencoderKL.from_single_file(url).cuda()
+
+    x = torch.randn(1, 3, 768, 768).cuda()
+    z, h = model.encode(x)
+    x_rec, h = model.decode(z.sample())
+    print(x_rec.shape)
+
+    print("Done")
--- vae_distillation/teacher_student.py
+++ vae_distillation/teacher_student.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 import torch.nn.functional as F
 from einops import reduce
 
@@ -7,6 +8,7 @@ from einops import reduce
 # For conv backbone VAEs only
 class TeacherStudent(nn.Module):
     def __init__(self, teacher_model, student_model):
+        super().__init__()
 
         self.teacher = teacher_model
         for param in self.teacher.parameters():
@@ -14,60 +16,107 @@ class TeacherStudent(nn.Module):
 
         self.student = student_model
 
-        # For simplicities sake we will make heavy assumptions about architectures
-        # Specifically, we will assume VAEs
-
-        # We will also assume the teacher model can output hidden states
-        # the channel lists below should correspond to layers
-        # the outputted hidden states should correspond to layers represented by channel counts
-
-        # We also assume the model encode forward passes return:
-        #   - latent, mu, logvar, hidden_states
+        # We also assume model encode forward passes return:
+        #   - latent_dist, hidden_states
         # And decoder returns
         #   - reconstruction, hidden_states
-        # NOTE: This probably requires a wrapper
+        # NOTE: This probably requires a wrapper around whatever model you want to use
+        # Latent dist is diffusers DiagonalGaussianDistribution
 
         # Boolean lists
         #  should given layer have features distilled?
         self.student_distill_features = {
-            "encoder" : []
-            "decoder" : []
+            "encoder" : [True] * 4,
+            "decoder" : [True] * 4
         }
 
         # loss weighting
-        self.feature_loss_weight = 0.1
-        self.kl_loss_weight = 0.1
-        self.z_rec_weight = 0.1
-        self.rec_weight = 0.1
+        self.feature_loss_weight = 1.0
+        self.kl_loss_weight = 1.0
+        self.z_rec_weight = 1.0
+        self.rec_weight = 1.0
+
+        # Which features in teacher are mapped to which features in student?
+        # You should know before hand how many layers are in the encoder
+        # and the decoder
+        self.feature_mapping_encoder = {
+
+        }
+        self.feature_mapping_decoder = None
+    
+    @torch.no_grad()
+    def mapping_help(self, sample):
+        """
+        Pretty prints all hidden shapes so you can find best features to match.
+        Reccomended to match two layers, and matching middle + later layers is 
+        more effective
+        """
+        print("===================================")
+        
+        teacher_dist, teacher_h = self.teacher.encode(sample)
+        latent_dist, h = self.student.encode(sample)
+
+        print("Teacher hidden layer shapes:")
+        for i, h_i in enumerate(teacher_h):
+            print(f"Layer {i}: {list(h_i.shape)}")
+        
+        print("Student hidden layer shapes:")
+        for i, h_i in enumerate(h):
+            print(f"Layer {i}: {list(h_i.shape)}")
+
+        z_teacher = teacher_dist.sample()
+        z_student = latent_dist.sample()
+
+        print("z_teacher shape:", z_teacher.shape)
+        print("z_student shape:", z_student.shape)
+
+        x_rec_teacher, h_teacher = self.teacher.decode(z_teacher)
+        x_rec_student, h_student = self.student.decode(z_student)
+
+        print("Teacher hidden layer shapes:")
+        for i, h_i in enumerate(h_teacher):
+            print(f"Layer {i}: {list(h_i.shape)}")
+
+        print("Student hidden layer shapes:")
+        for i, h_i in enumerate(h_student):
+            print(f"Layer {i}: {list(h_i.shape)}")
+
+        print("Teacher reconstructed x shape:", x_rec_teacher.shape)
+        print("Student reconstructed x shape:", x_rec_student.shape)
+
+        print("===================================")
+        exit()
     
     def forward(self, pixel_values):
         # Encoding
-        z_true, mu_true, logvar_true, h_true = self.teacher.encode(pixel_values, output_hidden_states = True)
-        z, mu, logvar, h = self.student.encode(pixel_values, output_hidden_states = True)
+        latent_dist_true, h_teacher = self.teacher.encode(pixel_values)
+        latent_dist, h = self.student.encode(pixel_values)
         
-        step_size = len(h_true) // len(h)
-        h_true_sampled = h_true[::step_size][:len(h)]
+        h_preds = []
+        h_targets = []
+
+        for key in self.feature_mapping_encoder:
+            h_preds.append(h[key])
+            h_targets.append(h_teacher[self.feature_mapping_encoder[key]])
+
+        h_targets = [h_true[idx] for idx in self.feature_mapping_encoder]
+        print(len(h))
+        print(len(h_targets))
+        exit()
 
         loss = 0
 
-        for idx in range(len(h)):
-            if self.student_distill_features['encoder'][idx]:
-                loss += self.feature_loss_weight * F.l1_loss(h[idx], h_true_sampled[idx])
+        for (h_i, h_i_target) in zip(h_preds, h_targets):
+            loss += self.feature_loss_weight * F.l1_loss(h[idx], h_targets[idx])
         
+        # Sample from both distributions 
+        z_true = latent_dist_true.sample()
+        z = latent_dist.sample
+
         # Align the latent distribution
-        loss += F.mse_loss()
 
         # KL divergence between two diagonal gaussians
-        d = torch.prod(torch.tensor(z.shape[1:]))
-        def d_sum(x):
-            return reduce(x, 'b ... -> b', reduction = 'sum')
-
-        kl_div = self.kl_loss_weight * 0.5 * (
-            d_sum(logar - logvar_true) - \
-            d + \
-            d_sum(logvar - logvar_true).exp() + \
-            d_sum((mu_true - mu).pow(2) * (1./logvar_true))
-        )
+        kl_div = self.kl_loss_weight * latent_dist.kl(latent_dist_true)
 
         z_rec_loss = self.z_rec_weight * F.mse_loss(z, z_true)
 
@@ -75,15 +124,18 @@ class TeacherStudent(nn.Module):
         loss += z_rec_loss
 
         # Decoding
-        rec_true, h_true = self.teacher.decode(z_true, output_hidden_states = True)
-        rec, h = self.student.decode(z, output_hidden_states = True)
+        rec_true, h_true = self.teacher.decode(z_true)
+        rec, h = self.student.decode(z)
 
-        step_size = len(h_true) // len(h)
-        h_true_sampled = h_true[::step_size][:len(h)]
+        if self.feature_mapping_decoder is None:
+            self.feature_mapping_decoder = self.lazy_find_mapping(
+                h, h_true
+            )
+        h_targets = [h_true[idx] for idx in self.feature_mapping_decoder]
 
         for idx in range(len(h)):
             if self.student_distill_features['decoder'][idx]:
-                loss += self.feature_loss_weight * F.l1_loss(h[idx], h_true_sampled[idx])
+                loss += self.feature_loss_weight * F.l1_loss(h[idx], h_targets[idx])
 
         rec_loss = self.rec_weight * self.rec_loss(rec, rec_true)
 
@@ -91,4 +143,26 @@ class TeacherStudent(nn.Module):
 
         return loss
 
+if __name__ == "__main__":
+    from .adapt_diffusers import HiddenStateAutoencoderKL
+    from diffusers import AutoencoderKL
+    import torch
 
+    sd_vae_path = "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/blob/main/vae-ft-mse-840000-ema-pruned.safetensors"
+
+    model = TeacherStudent(
+        teacher_model = HiddenStateAutoencoderKL.from_single_file(sd_vae_path),
+        student_model = HiddenStateAutoencoderKL.cast(AutoencoderKL(
+            down_block_types = ('DownEncoderBlock2D',)*2,
+            block_out_channels = (128, 512),
+            up_block_types = ('UpDecoderBlock2D',)*2,
+            sample_size = 768, layers_per_block = 1,
+            latent_channels = 4
+        ))
+    ).cuda()
+    model.mapping_help(torch.randn(1,3,768,768).cuda())
+    exit()
+
+    rand_image = torch.randn(1, 3, 768, 768).cuda()
+    loss = model(rand_image)
+    print(loss)
