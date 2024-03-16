d797f570918168a0462f177d89a27b5838a3bbc9
Shahbuland Matiana <smatiana1@gmail.com>
2024-03-16 17:55:31 +0000

    Data common scripts

---
+++ common/data/collators.py
@@ -0,0 +1,59 @@
+"""
+This script contains common/generic collators for model inputs
+"""
+
+from typing import Iterable, Union, Tuple
+
+from PIL import Image
+
+from transformers import AutoProcessor, AutoTokenizer
+
+class ImageCollator:
+    def __init__(self):
+        clip_id = "laion/CLIP-ViT-g-14-laion2B-s12B-b42K"
+        self.processor = AutoProcessor.from_pretrained(clip_id)
+
+    def __call__(self, image_batch: Iterable[Image.Image]):
+        return {
+            "pixel_values" : self.processor(image_batch).pixel_values
+        }
+
+class ImageTextCollator:
+    def __init__(self, tokenizer = None):
+        self.img_c = ImageCollator
+
+        clip_id = "laion/CLIP-ViT-g-14-laion2B-s12B-b42K"
+        if isinstance(tokenizer, str): # Interpret is as an identifier
+            self.tokenizer = AutoTokenizer.from_pretrained(tokenizer) 
+        elif tokenizer is not None: # It was passed
+            self.tokenizer = tokenizer
+        else: # reasonable random init
+            self.tokenizer = AutoTokenizer.from_pretrained(clip_id)
+
+    def __call__(self, batch : Iterable[Tuple]):
+        """
+        Image/text collator that works agnostic to whether batches are presented (image, str) or (str, image)
+        """
+        shift = 0
+        if not isinstance(batch[0][0], Image.Image):
+            shift = 1 # shift if caption first
+
+        images = [item[shift] for item in batch]
+        texts = [item[1-shift] for item in batch]
+
+        images_out = self.img_c(images)
+        tok_out = self.tokenizer(
+            texts,
+            return_tensors = 'pt',
+            padding = "max_length", # Just standard diffusion settings
+            truncation = True,
+            max_length = 77
+        )
+
+        return images_out + {
+            "input_ids" : tok_out.input_ids,
+            "attention_mask" : tok_out.attention_mask
+        }
+
+
+        
