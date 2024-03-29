15725311c9810d94fd18563392e98200d09d5e71
Shahbuland Matiana <smatiana1@gmail.com>
2024-03-29 16:15:39 +0000

    Add filtering/output keys for S3 datasets

--- common/data/s3_datasets.py
+++ common/data/s3_datasets.py
@@ -1,6 +1,6 @@
-from typing import Tuple
+from typing import Tuple, List, Dict, Callable, Any
 
-from torch.utils.data import IterableDataset
+from torch.utils.data import IterableDataset, get_worker_info
 
 import tarfile
 from tarfile import TarFile
@@ -8,6 +8,8 @@ from PIL import Image
 import json
 from io import BytesIO
 import boto3
+import os
+import random
 
 def read_from_tar(tar, path):
     file = tar.extractfile(path)
@@ -38,8 +40,13 @@ ext_decode_map = {
 class S3Dataset(IterableDataset):
     """
     :param s3_url: URL to S3 dataset
+    
     :param filter_keys: Iterable of keys within tar files that we are actually interested in
     (keys = file extensions or common suffixes, ala .jpg or _chosen.jpg)
+    :param filter_fn: For each key, we will apply the corresponding filter function. The
+    data item will be skipped if it doesn't pass the filter for that key. We will still decode. 
+    :param output_keys: If given, use this for the actual dataset items. Useful for skipping items that are only for filtering
+
     :param decodes: Iterable of functions that can be applied to files obtained from above keys
     (if this is not passed, we can try to guess from keys (i.e. if they're text, or json or jpg)
     :param return_list: By default we return dict with keys mapped to respective data
@@ -47,12 +54,35 @@ class S3Dataset(IterableDataset):
 
     :param offset: Offset dataset iteration within tarfiles? Needed for multiproc. Must provide as
     tuple of (n_processes, process_id)
+    :param shard_shuffle: Assumes the top most level in the bucket is 
+    :param shard_ids:  
     """
-    def __init__(self, s3_url, filter_keys, decodes = None, return_list = True, offset : Tuple = None):
+    def __init__(
+        self,
+        s3_url,
+        filter_keys : List[str], filter_fns : Dict[str, Callable[[Any], bool]] = {},
+        output_keys : List[str] = None,
+        decodes = None, return_list = True,
+        offset : Tuple = None,
+        shard_shuffle : bool = False,
+        shard_ids : List[str] = None,
+        
+    ):
         s3 = boto3.resource('s3')
         bucket_name, prefix = s3_url.replace("s3://", "").split("/", 1)
-        bucket = s3.Bucket(bucket_name)
-        self.objects = iter(bucket.objects.filter(Prefix=prefix))
+        self.bucket = s3.Bucket(bucket_name)
+        self.filter_fns = filter_fns
+
+        self.shard_shuffle = shard_shuffle
+        self.prefixes = None
+
+        if shard_shuffle:
+            # Shuffle will be called on iteration
+            self.prefixes = [os.path.join(prefix, shard_id) for shard_id in shard_ids]
+            random.shuffle(shard_ids)
+            self.objects = None
+        else:
+            self.objects = iter(self.bucket.objects.filter(Prefix=prefix))
 
         self.offset = offset
 
@@ -68,9 +98,23 @@ class S3Dataset(IterableDataset):
         self.decodes = decodes
         self.return_list = return_list
 
+        if output_keys is None:
+            self.output_keys = filter_keys
+        else:
+            self.output_keys = output_keys
+
         # Cache is dict (over keys) of iterators over current tar file
         # There is a unique iterator for each key
-        self.tar_cache = self.get_next_tar()
+        self.tar_cache = None
+
+    def shuffle_shards(self, seed : int = None):
+        if seed is not None: random.seed(seed)
+        random.shuffle(self.prefixes)
+        self.prefixes = iter(self.prefixes)
+
+    def get_next_shard(self):
+        assert self.shard_shuffle
+        self.objects = iter(self.bucket.objects.filter(Prefix=next(self.prefixes)))
 
     def check_tar_number(self, key) -> int:
         """
@@ -101,23 +145,37 @@ class S3Dataset(IterableDataset):
                     }
 
         except StopIteration:
-            return None
+            if self.shard_shuffle:
+                try:
+                    self.get_next_shard()
+                    return self.get_next_tar()
+                except StopIteration:
+                    return None
+            else:
+                return None
     
     def get_next(self):
         while True:
             try:
+                res = {
+                    key : decode(next(self.tar_cache[key])) for decode, key in zip(self.decodes, self.filter_keys)
+                }
+                skip_this = False
+                for key in self.filter_fns:
+                    if not self.filter_fns[key](res[key]):
+                        skip_this = True # Didn't pass filter, continue
+                        break # Can't continue in this for loop, need to do it outside
+                if skip_this:
+                    continue
+
+                # Only use output keys
+                res = {k : res[k] for k in self.output_keys}
+                
                 if self.return_list:
-                    res = [
-                        decode(next(self.tar_cache[key])) for decode, key in zip(self.decodes, self.filter_keys)
-                    ]
+                    res = list(res.values())
                     if len(res) == 1:
                         res = res[0]
-                else:
-                    res = {
-                        key : decode(
-                            next(self.tar_cache[key])
-                        ) for decode, key in zip(self.decodes, self.filter_keys)
-                    }
+                
                 yield res
 
             except StopIteration:
@@ -128,4 +186,14 @@ class S3Dataset(IterableDataset):
                     continue
     
     def __iter__(self):
+        if self.shard_shuffle:
+            info = get_worker_info()
+            if info is not None:
+                seed = info.seed
+            else:
+                seed = None
+            self.shuffle_shards(seed)
+            self.get_next_shard()
+            self.tar_cache = self.get_next_tar()
+        
         return iter(self.get_next())
