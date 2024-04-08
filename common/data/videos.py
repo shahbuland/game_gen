9de6a223a612c62a035bd69bcf94e73be5136264
Shahbuland Matiana <smatiana1@gmail.com>
2024-04-08 02:12:40 +0000

    Use torchaudio stream for faster video loading (8x decord :D)

--- common/data/videos.py
+++ common/data/videos.py
@@ -7,40 +7,46 @@ import os
 import torch
 import einops as eo
 
-import decord
-from decord import VideoLoader, gpu, cpu
-decord.bridge.set_bridge('torch')
+from torchaudio.io import StreamReader
 
-# Alternate version of the above that saves the video to a tmp file, then reads and deletes
-def read_video(video_bytes : BytesIO, target_frames = 100, img_size = 256):
+def yuv_2_rgb(videos):
+    videos = videos.float()
+    y = videos[...,0,:,:] / 255
+    u = videos[...,1,:,:] / 255 - 0.5
+    v = videos[...,2,:,:] / 255 - 0.5
+
+    res = torch.empty_like(videos)
+    res[...,0,:,:] = y + 1.14 * v # r
+    res[...,1,:,:] = y + -0.396 * u - 0.581 * v # g
+    res[...,2,:,:] = y + 0.2029 * u # b
+
+    return res
+
+def read_video(video_bytes : BytesIO, gpu_idx : int = 0, img_size = 256, chunk_size = 50):
     """
-    Using decord, read a video from a file path in a given size and FPS
-    Retains duration of the original video and uses FPS given to skip frames rather than as an assumption of duration.
+    Read video from BytesIO wih nvdec
 
-    :param path: File path to video
-    :param fps: Video is temporally downsampled to be this FPS
-    :size: Tuple for image size of each frame
+    :param video_bytes: BytesIO object for MP4 video
+    :param gpu_idx: Index for GPU to use
+    :target_frames
     """
-    try:
-        vr = decord.VideoReader(video_bytes, width = img_size, height = img_size, 
-            ctx = cpu(),
-            num_threads = 2
-        )
-    except:
-        # Bad video, try returning an empty batch item
-        print("Bad batch detected, ignoring")
-        return torch.zeros(0, target_frames, img_size, img_size, 3, dtype = torch.uint8)
+    config = {
+        'decoder' : 'h264_cuvid',
+        'hw_accel' : f'cuda:{gpu_idx}',
+        'decoder_option' : {
+            'resize' : f'{img_size}x{img_size}'
+        }
+    }
 
-    max_frames = len(vr)
+    s = StreamReader(video_bytes.getvalue())
+    s.add_video_stream(chunk_size, **config)
+    chunks = []
+    for i, (chunk, ) in enumerate(s.stream()):
+        chunks.append(chunk)
 
-    scale_factor = max_frames / target_frames
-    inds = list(range(target_frames))
-    if max_frames != target_frames:
-        inds = [int(i * scale_factor) for i in inds]
+    chunks = torch.cat(chunks)
 
-    frames = vr.get_batch(inds)
-    
-    return frames.unsqueeze(0) # add batch dim
+    return chunks
 
 class VideoCollator:
     """
@@ -52,6 +58,7 @@ class VideoCollator:
         processor = None,
         img_size : int = None,
         target_frames : int = None,
+        gpu_idx : int = 0
     ):
         if processor is None:
             assert target_frames is not None, "Default processor needs target_frames to be passed to collator"
@@ -59,17 +66,32 @@ class VideoCollator:
 
         self.img_size = img_size
         self.target_frames = target_frames
+        self.process_idx = gpu_idx
+
+    def temporal_resample(self, video):
+        n_frames, c, h, w = video.shape
+        ratio = n_frames / self.target_frames
+
+        res = torch.empty(self.target_frames, c, h, w, device = video.device, dtype = video.dtype)
+        for i in range(self.target_frames):
+            res[i] = video[min(round(i*ratio), n_frames - 1)]
+        
+        return res
 
     def __call__(self, video_bytes : List[BytesIO]):
         video_list = []
         videos = []
 
         for i, vid in enumerate(video_bytes):
-            videos.append(read_video(vid, target_frames = self.target_frames, img_size = self.img_size))
+            try:
+                res_vid = read_video(vid, self.process_idx, img_size = self.img_size)
+                res_vid = self.temporal_resample(res_vid)
+                videos.append(res_vid)
+            except:
+                print("Bad video detected. Ignoring.")
+            
         
-        videos = torch.cat(videos)
-        videos = eo.rearrange(videos, 'b t h w c -> b t c h w').contiguous()
-        videos = self.processor(videos)
+        videos = yuv_2_rgb(torch.stack(videos))
         return {
             "pixel_values" : videos
         }
