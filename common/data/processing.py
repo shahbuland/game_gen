e508001a33e05b5e3e3b244287617a50004aab29
shahbuland <smatiana1@gmail.com>
2024-07-01 04:09:26 -0400

    Add a standard processor (not clip)

--- common/data/processing.py
+++ common/data/processing.py
@@ -12,9 +12,9 @@ import numpy as np
 clip_means = [0.48145466, 0.4578275, 0.40821073]
 clip_stds = [0.26862954, 0.26130258, 0.27577711]
 
-def common_image_preprocessor(images = None, img_size=224, resampling = 3):
+def clip_image_preprocessor(images = None, img_size=224, resampling = 3):
     """
-    Simple image preprocessor that can be used directly as callable or to create
+    Image procesor using CLIP means/stds that can be used directly as callable or to create
     a preprocessing function. If no images are passed, this returns a callable.
     """
     transform = TF.Compose([
@@ -31,9 +31,36 @@ def common_image_preprocessor(images = None, img_size=224, resampling = 3):
     return torch.stack([transform(img) for img in images])
 
 @torch.no_grad()
-def common_image_postprocessor(model_out, to_pil = False):
+def clip_image_postprocessor(model_out, to_pil = False):
     for i in range(3):
         model_out[:,i] = model_out[:,i] * clip_stds[i] + clip_means[i]
+    model_out = model_out.clamp(0, 1)
+    model_out = model_out.permute(0, 2, 3, 1)
+    model_out = (model_out.float() * 255).detach().cpu().numpy().astype(np.uint8)
+    return [Image.fromarray(img) if to_pil else img for img in model_out]
+
+def common_image_preprocessor(images = None, img_size=224, resampling = 3):
+    """
+    Simple image preprocessor that can be used directly as callable or to create
+    a preprocessing function. If no images are passed, this returns a callable.
+    """
+    transform = TF.Compose([
+        TF.Resize((img_size, img_size), interpolation = resampling),
+        TF.ToTensor(),
+        TF.Normalize(
+            [0.5, 0.5, 0.5],
+            [0.5, 0.5, 0.5]
+        )
+    ])
+
+    if images is None:
+        return lambda x : torch.stack([transform(x_i) for x_i in x])
+    return torch.stack([transform(img) for img in images])
+
+@torch.no_grad()
+def common_image_postprocessor(model_out, to_pil = False):
+    for i in range(3):
+        model_out[:,i] = model_out[:,i] * 0.5 + 0.5
     model_out = model_out.clamp(0, 1)
     model_out = model_out.permute(0, 2, 3, 1)
     model_out = (model_out.float() * 255).detach().cpu().numpy().astype(np.uint8)
